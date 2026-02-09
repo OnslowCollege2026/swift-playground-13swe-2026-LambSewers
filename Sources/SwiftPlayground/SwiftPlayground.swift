e5f7980f0e8cfe63466fac77bafcb731d95bb875
LamSewers <sam.lewers@student.onslow.school.nz>
2026-02-09 01:28:51 +0000

    Finished Revision task

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,45 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        let lunches = [6.50, 8.00, 5.75, 9.20, 7.10]
+        let budget = 35.00
+
+        func totalCost(prices: [Double]) -> Double {
+            var totalCost: Double = 0
+
+            for price in prices {
+                totalCost += price
+            }
+            return totalCost
+        }
+
+        func isOverBudget(total: Double, budget: Double) -> Bool {
+            if total >= budget {
+                return true
+            }
+            return false
+        }
+
+        func averageCost(prices: [Double], ) -> Double {
+            let totalCost = totalCost(prices: prices)
+            let numItems = prices.count
+            return totalCost / Double(numItems)
+        }
+
+        for index in 0..<lunches.count {
+            print("Day \(index): $\(lunches[index])")
+        }
+
+        let totalCost = (totalCost(prices: lunches))
+        print("Total weekly total: $\(totalCost)")
+        print("Average cost of lunch: $\(averageCost(prices: lunches))")
+
+
+        if !isOverBudget(total: totalCost, budget: budget){
+            print("You stayed within budget.")
+        } else {
+            print("Warning: You overspent this week.")
+        }
+
     }
 }
