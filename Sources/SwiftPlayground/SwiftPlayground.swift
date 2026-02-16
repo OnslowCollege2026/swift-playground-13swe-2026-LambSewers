d0c04819e8e937f18431c3789513e98a48889e8b
LamSewers <sam.lewers@student.onslow.school.nz>
2026-02-16 02:05:27 +0000

    Finished task B

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,78 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        let mixed = ["cat", "7", "owl", "15", "dog", "3"]
+
+        let probablyNumbers = mixed.compactMap {Int($0)}
+        let ifNumbersAreNumbers = mixed.allSatisfy{Int($0) != nil}
+
+        print(probablyNumbers)
+        print("Does mixed contain only numbers \(ifNumbersAreNumbers)")
+
+
+
+
+        let sightings = [
+        (name: "moth", score: 3),
+        (name: "wolf", score: 9),
+        (name: "raven", score: 4),
+        (name: "mist", score: 7),
+        (name: "wisp", score: 2)
+        ]
+
+        // Looks at the first letter of the first item of the list and checks if it is M or W
+        let filteredSightings = sightings.filter {$0.0.first == "m" || $0.0.first == "w"} 
+
+
+
+
+        // let sightingScores = filteredSightings.compactMap {Int($0.0)}
+        // Makes a new array that only contains the what is after score: 
+        let sightingScores = filteredSightings.map {$0.score}
+
+        let totalSightingScores = sightingScores.reduce(0,+)
+
+        let minScore = sightingScores.min {$0 < $1}
+
+        let maxScore = sightingScores.max {$0 < $1}
+
+
+        print("Filtered sightings \(filteredSightings)")
+        print(sightingScores)
+        print(totalSightingScores)
+        print("Min sighting score: \(minScore!)")
+        print("Max sighting score: \(maxScore!)")
+
+
+
+
+
+
+
+
+
+        let archive = [
+            [
+                [["candle", "dust"], ["mirror", "ash"]],
+                [["whisper", "shadow"], ["clock", "veil"]]
+            ],
+            [
+                [["stone", "key"], ["relic", "name"]],
+                [["cipher", "bone"], ["ember", "seal"]]
+            ]
+        ]
+
+
+        // let firstLastLastLastItem = archive.last?.last?.last?.first
+
+
+
+
+       // print("The first item of the last item of the last item of the last item of the archive is: \(firstLastLastLastItem)")
+
     }
 }
+
+
+
+
